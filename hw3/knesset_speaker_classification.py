026179dd0eaf78531d5137021d22b99c2f595165
ObaidaKhateeb <okhati01@campus.haifa.ac.il>
2024-12-25 15:27:36 +0200

    section 2 update

--- hw3/knesset_speaker_classification.py
+++ hw3/knesset_speaker_classification.py
@@ -23,34 +23,34 @@ def json_lines_extract(file):
 
 # A method that splits the sentences according to the speaker (section 1.2)
 def split_data_by_speaker(json_lines, speaker1, speaker2):
-    speaker1_sentences = []
-    speaker2_sentences = []
-    others_sentences = []
+    speaker1_data = []
+    speaker2_data = []
+    others_data = []
     speaker1_splitted = speaker1.split()
     speaker2_splitted = speaker2.split()
     for line in json_lines:
         speaker = line['speaker_name']
         if speaker == speaker1:
-            speaker1_sentences.append(line['sentence_text'])
+            speaker1_data.append(line)
         elif speaker == speaker2:
-            speaker2_sentences.append(line['sentence_text'])
+            speaker2_data.append(line)
         else:
             speaker_splitted = speaker.split()
             
             #checking if the sentence said by one of the two speakers but with a different given name
             if len(speaker_splitted) == 1 and speaker_splitted[0] == speaker1_splitted[-1]:
-                speaker1_sentences.append(line['sentence_text'])
+                speaker1_data.append(line)
             elif len(speaker_splitted) == 1 and speaker_splitted[0] == speaker2_splitted[-1]:
-                    speaker2_sentences.append(line['sentence_text'])
+                    speaker2_data.append(line)
             elif len(speaker_splitted) > 1 and  speaker_splitted[0][0] == speaker1_splitted[0][0] and speaker_splitted[-1] == speaker1_splitted[-1]:
-                speaker1_sentences.append(line['sentence_text'])
+                speaker1_data.append(line)
             elif len(speaker_splitted) > 1 and speaker_splitted[0][0] == speaker2_splitted[0][0] and speaker_splitted[-1] == speaker2_splitted[-1]:
-                    speaker2_sentences.append(line['sentence_text'])
+                    speaker2_data.append(line)
             
             #the case where the sentence supposed to be said by someone other than the two
             else:
-                others_sentences.append(line['sentence_text'])
-    return speaker1_sentences, speaker2_sentences, others_sentences
+                others_data.append(line)
+    return speaker1_data, speaker2_data, others_data
 
 def main():
     file = 'knesset_corpus.jsonl'
@@ -61,22 +61,23 @@ def main():
     #print(top_speakers)
 
     #split the data according to the speaker (section 1.2)
-    rivlin_total_sentences, burg_total_sentences, others_total_sentences = split_data_by_speaker(json_lines, "ראובן ריבלין", "א' בורג")
+    rivlin_full_data, burg_full_data, others_full_data = split_data_by_speaker(json_lines, "ראובן ריבלין", "א' בורג")
     
     #classes balancing (section 2)
-    class_count = min(len(rivlin_total_sentences), len(burg_total_sentences), len(others_total_sentences))
-    rivlin_sentences = random.sample(rivlin_total_sentences, class_count)
-    burg_sentences = random.sample(burg_total_sentences, class_count)
-    others_sentences = random.sample(others_total_sentences, class_count)
+    class_count = min(len(rivlin_full_data), len(burg_full_data), len(others_full_data))
+    rivlin_data = random.sample(rivlin_full_data, class_count)
+    burg_data = random.sample(burg_full_data, class_count)
+    others_data = random.sample(others_full_data, class_count)
 
-    #printing the count of sentences of each class before and after the down sampling (section 2)
+    # #printing the count of sentences of each class before and after the down sampling (section 2)
     # print('Sentences count of each class before the down sampling:')
-    # print('rivlin_sentences:', len(rivlin_total_sentences))
-    # print('burg_sentences:', len(burg_total_sentences))
-    # print('others_sentences:', len(others_total_sentences))
+    # print('rivlin_sentences:', len(rivlin_full_data))
+    # print('burg_sentences:', len(burg_full_data))
+    # print('others_sentences:', len(others_full_data))
     # print('Sentences count of each class after the down sampling:')
-    # print('rivlin_sentences:', len(rivlin_sentences))
-    # print('burg_sentences:', len(burg_sentences))
-    # print('others_sentences:', len(others_sentences))
+    # print('rivlin_sentences:', len(rivlin_data))
+    # print('burg_sentences:', len(burg_data))
+    # print('others_sentences:', len(others_data))
+
 if __name__ == '__main__':
     main()
