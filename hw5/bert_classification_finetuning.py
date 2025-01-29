4b15ed8e2bef681f487d701e570dc1cd7cd72656
ObaidaKhateeb <okhati01@campus.haifa.ac.il>
2025-01-30 01:00:13 +0200

    update

--- hw5/bert_classification_finetuning.py
+++ hw5/bert_classification_finetuning.py
@@ -56,13 +56,13 @@ def main():
     val_set = subset["test"]
 
     #training the model (section 2.4)
-    training_args = TrainingArguments(output_dir= './results', evaluation_strategy = "no", learning_rate= 2e-5, per_device_train_batch_size = 8, per_device_eval_batch_size = 8, num_train_epochs = 3, weight_decay = 0.01)
+    training_args = TrainingArguments(output_dir= './bert_results', evaluation_strategy = "no", learning_rate= 2e-5, per_device_train_batch_size = 8, per_device_eval_batch_size = 8, num_train_epochs = 3, weight_decay = 0.01)
     trainer = Trainer(model = model, args = training_args, train_dataset = train_set, eval_dataset = val_set, tokenizer = tokenizer, compute_metrics=compute_metrics)
     trainer.train()
 
     #evaluating the model (section 2.5) 
     accuracy = trainer.evaluate()
-    print(accuracy)
+    print(accuracy["eval_accuracy"])
 
 if __name__ == "__main__":
     main()
